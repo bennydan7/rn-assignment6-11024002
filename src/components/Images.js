9853d87959e2472117589b223e3b61f7393c1dda
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-07-03 09:25:46 +0000

    created images component

---
+++ src/components/Images.js
@@ -0,0 +1,19 @@
+const Images = {
+  add_circle: require("../../assets/appImages/add_circle.png"),
+  dress1: require("../../assets/appImages/dress1.png"),
+  dress2: require("../../assets/appImages/dress2.png"),
+  dress3: require("../../assets/appImages/dress3.png"),
+  dress4: require("../../assets/appImages/dress4.png"),
+  dress5: require("../../assets/appImages/dress5.png"),
+  dress6: require("../../assets/appImages/dress6.png"),
+  dress7: require("../../assets/appImages/dress7.png"),
+  Filter: require("../../assets/appImages/Filter.png"),
+  ListView: require("../../assets/appImages/ListView.png"),
+  Logo: require("../../assets/appImages/Logo.png"),
+  Menu: require("../../assets/appImages/Menu.png"),
+  remove: require("../../assets/appImages/remove.png"),
+  Search: require("../../assets/appImages/Search.png"),
+  ShoppingBag: require("../../assets/appImages/ShoppingBag.png"),
+};
+
+export default Images;
