6f7600b6f48f59fc76609f71908d61a046fa8781
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-07-03 11:31:30 +0000

    modified main app

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { NavigationContainer, StackRouter } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import Checkout from "./src/screens/Checkout";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CheckoutScreen" component={Checkout} />
       </Stack.Navigator>
     </NavigationContainer>
   );
