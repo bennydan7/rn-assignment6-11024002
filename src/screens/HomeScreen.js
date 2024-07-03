18aad28eefa28e8cb8bfd7263623a5912821cbba
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-07-03 09:28:00 +0000

    created homescreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  return <Text>import React from "react";</Text>;
+};
+
+export default HomeScreen;
